ad1f3d675efd16ae370280c8a8e01e830646667c
akash dey <dey3552@gmail.com>
2026-03-23 21:55:31 +0530

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/jeeva_gold/',
+  plugins: [react()]
 })
